6d7e9b639f384e60e47616b12f8c2aab473dd6dc
Martin Le <martinle@outlook.com>
2018-12-02 11:57:50 -0800

    new start

---
+++ src/keyboard/Keyboard.java
@@ -0,0 +1,55 @@
+/**
+ * @authors Yorick, Glen, Martin
+ */
+package keyboard;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.midi.MidiUnavailableException;
+
+public class Keyboard {
+
+    public static void main(String[] args) {
+         /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(KeyboardView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        //</editor-fold>
+
+        KeyboardView view = new KeyboardView();
+        KeyboardModel model = null;
+        
+        try 
+        {
+            model = new KeyboardModel();
+        } 
+        catch (MidiUnavailableException ex) 
+        {
+            javax.swing.JOptionPane.showMessageDialog(null, ex.getStackTrace(), "Unable to open up MIDI: " + ex, 0);
+            Logger.getLogger(Keyboard.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(0);
+        }
+        KeyboardController controller = new KeyboardController(model, view);
+        view.addMouseListener(controller);
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable(){ 
+            public void run() { view.setVisible(true); } 
+        });//end of EventQueue.invokeLater()
+        
+    }//end of main()
+    
+}
---
+++ src/keyboard/KeyboardController.java
@@ -0,0 +1,132 @@
+package keyboard;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.*;
+
+
+public class KeyboardController implements MouseListener{
+    public static final int DEFAULT_OCTAVE = 4;
+    private KeyboardModel model;
+    private KeyboardView view;
+
+    
+    public KeyboardController(KeyboardModel model, KeyboardView view){
+        this.model = model;
+        this.view = view;
+    }
+    
+    public void mousePressed(MouseEvent e){
+        KeyboardModel.Note note = view.getNote(e.getSource() );
+        model.startNote(DEFAULT_OCTAVE, note);
+        view.setKeyColor(note, Color.YELLOW);
+    }//end of mousePressed
+
+    public void mouseReleased(MouseEvent e){
+        KeyboardModel.Note note = view.getNote(e.getSource() );
+        model.stopNote(DEFAULT_OCTAVE, note);
+        switch(note){
+            case Csharp:    
+            case Dsharp:    
+            case Fsharp:    
+            case Gsharp:    
+            case Asharp:    
+                view.setKeyColor(note, Color.BLACK);
+                break;
+            case C:
+            case D:
+            case E:
+            case F:
+            case G:
+            case A:
+            case B:
+                view.setKeyColor(note, Color.WHITE);
+                break;
+        }
+    }//end of mousereleased
+
+    @Override 
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+    
+    private static class FakeKeyboardModel extends KeyboardModel{
+        private int octave;
+        private Note note;
+        private boolean noteOn;
+        public FakeKeyboardModel(){
+            super(new KeyboardModel.FakeMidiChannel() );
+        }
+        
+        public void startNote(int octave, Note note){
+            this.octave = octave;
+            this.note = note;
+            this.noteOn = true;
+        }
+        public void stopNote(int octave, Note note){
+            this.octave = octave;
+            this.note = note;
+            this.noteOn = false;
+        }
+        public boolean expect(int octave, Note note, boolean noteOn) {
+            return this.octave == octave && this.note==note && this.noteOn == this.noteOn;
+        }
+    }
+    
+    private static class FakeKeyboardView extends KeyboardView{
+        private KeyboardModel.Note note, expectedNote;
+        private Color color;
+        private Object source;
+        public void setKeyColor(KeyboardModel.Note note, Color color){
+            this.note = note;
+            this.color = color;
+        }
+        public KeyboardModel.Note getNote(Object source){
+            this.source = source;
+            return expectedNote;
+        }
+        public void setExpectedNote(KeyboardModel.Note note){
+            this.expectedNote = note;
+        }
+        public boolean expect(Object source){
+            return this.source == source;
+        }
+        public boolean expect(KeyboardModel.Note note, Color color){
+            return this.note == note && this.color == color;
+        }
+    }
+    
+    public static void main(String args[]){
+        FakeKeyboardModel model = new FakeKeyboardModel();
+        FakeKeyboardView view = new FakeKeyboardView();
+        KeyboardController controller = new KeyboardController(model, view);
+        Component c = new Button();
+        
+        // C Key is pressed.
+        view.setExpectedNote(KeyboardModel.Note.C);
+        controller.mousePressed(new MouseEvent(c, 0, 0, 0, 0, 0, 1, false));
+        System.out.println(model.expect(4, KeyboardModel.Note.C, true));
+        System.out.println(view.expect(KeyboardModel.Note.C, Color.YELLOW));
+        System.out.println(view.expect(c));
+        
+        // C Key is released.
+        view.setExpectedNote(KeyboardModel.Note.C);
+        controller.mouseReleased(new MouseEvent(c, 0, 0, 0, 0, 0, 1, false));
+        System.out.println(model.expect(4, KeyboardModel.Note.C, false));
+        System.out.println(view.expect(KeyboardModel.Note.C, Color.WHITE));
+        System.out.println(view.expect(c));
+        
+        // F-sharp is released.
+        view.setExpectedNote(KeyboardModel.Note.Fsharp);
+        controller.mouseReleased(new MouseEvent(c, 0, 0, 0, 0, 0, 1, false));
+        System.out.println(model.expect(4, KeyboardModel.Note.Fsharp, false));
+        System.out.println(view.expect(KeyboardModel.Note.Fsharp, Color.BLACK));
+        System.out.println(view.expect(c));
+
+    }//end of main
+    
+}//end of class KeyboardController
