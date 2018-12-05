69ecbad0121e19d8425618abf21e186ad0cd5593
David Johnson <dbjtemplarreccos@gmail.com>
2018-12-04 19:49:17 -0800

    Revert "Enabled Instrument Selector."
    This reverts commit f8e35f005502eb262e77e7fd0ed9803d594dc23f.

--- MusicPlayer.java
+++
@@ -1,6 +0,0 @@
-package keyboard;
-
-public interface MusicPlayer {
-    public void startNote(int octave, KeyboardModel.Note note);
-    public void stopNote(int octave, KeyboardModel.Note note);
-}
