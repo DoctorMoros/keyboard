c84ee9aa0c3ccbd636375c1d7d2d75582b385cb5
David Johnson <dbjtemplarreccos@gmail.com>
2018-12-05 01:47:52 -0800

    Test branch merging by deleting old file.

--- src/keyboard/Instrument.java
+++
@@ -1,9 +0,0 @@
-package Keyboard;
-
-import Keyboard.KeyboardModel;
-
-public interface Instrument {
-        
-    public void startNote(int octave, KeyboardModel.Note note);
-    public void stopNote(int octave, KeyboardModel.Note note);
-}
